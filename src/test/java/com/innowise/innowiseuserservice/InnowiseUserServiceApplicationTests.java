3b787d922da24cbf52f617d082685d594a67981c
Katya Remenchik <164913748+katermnchk@users.noreply.github.com>
2025-09-15 21:15:29 +0300

    Initial commit

---
+++ src/test/java/com/innowise/innowiseuserservice/InnowiseUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innowise.innowiseuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnowiseUserServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
