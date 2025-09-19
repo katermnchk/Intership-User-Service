c0ad2b756bbeb6bd3d50ca0cf5152f0b748d3329
Katya Remenchik <164913748+katermnchk@users.noreply.github.com>
2025-09-19 21:09:07 +0300

    clean up repositories

--- src/main/java/com/innowise/innowiseuserservice/repository/UserRepository.java
+++ src/main/java/com/innowise/innowiseuserservice/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.innowise.innowiseuserservice.repository;
 
 import com.innowise.innowiseuserservice.entity.User;
-import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,20 +12,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-  @Query(
-      value = "SELECT * FROM users WHERE id = :id",
-      nativeQuery = true
-  )
-  Optional<User> findUserById(@Param("id") Long id);
-
   List<User> findAllByIdIn(List<Long> ids);
 
   @Query("SELECT u FROM User u WHERE u.email = :email")
   Optional<User> findByEmail(@Param("email") String email);
 
-
   @Modifying
-  @Transactional
   @Query("UPDATE User u SET u.name = :name, u.surname = :surname, "
       + "u.birthDate = :birthDate, u.email = :email WHERE u.id = :id")
   void updateUserById(@Param("id") Long id,
@@ -35,11 +26,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
                       @Param("birthDate") java.time.LocalDate birthDate,
                       @Param("email") String email);
 
-  @Modifying
-  @Transactional
-  @Query(
-      value = "DELETE FROM users WHERE id = :id",
-      nativeQuery = true
-  )
-  void deleteUserById(@Param("id") Long id);
 }
