b1afc6781f9089d94c72c7404dc6581a13c9bd49
Katya Remenchik <164913748+katermnchk@users.noreply.github.com>
2025-09-20 14:17:22 +0300

    remove unnecessary  methods from repositories

--- src/main/java/com/innowise/innowiseuserservice/repository/CardInfoRepository.java
+++ src/main/java/com/innowise/innowiseuserservice/repository/CardInfoRepository.java
@@ -3,22 +3,11 @@ package com.innowise.innowiseuserservice.repository;
 import com.innowise.innowiseuserservice.entity.CardInfo;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CardInfoRepository extends JpaRepository<CardInfo, Long> {
 
   List<CardInfo> findAllByIdIn(List<Long> ids);
-
-  @Modifying
-  @Query("UPDATE CardInfo c SET c.cardNumber = :cardNumber,c.cardHolderName = :holder, "
-      + "c.expirationDate = :expirationDate WHERE c.id = :id")
-  void updateCardById(@Param("id") Long id,
-                      @Param("cardNumber") String cardNumber,
-                      @Param("holder") String holder,
-                      @Param("expirationDate") String expirationDate);
 
 }
--- src/main/java/com/innowise/innowiseuserservice/repository/UserRepository.java
+++ src/main/java/com/innowise/innowiseuserservice/repository/UserRepository.java
@@ -4,8 +4,6 @@ import com.innowise.innowiseuserservice.entity.User;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -14,16 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
   List<User> findAllByIdIn(List<Long> ids);
 
-  @Query("SELECT u FROM User u WHERE u.email = :email")
-  Optional<User> findByEmail(@Param("email") String email);
-
-  @Modifying
-  @Query("UPDATE User u SET u.name = :name, u.surname = :surname, "
-      + "u.birthDate = :birthDate, u.email = :email WHERE u.id = :id")
-  void updateUserById(@Param("id") Long id,
-                      @Param("name") String name,
-                      @Param("surname") String surname,
-                      @Param("birthDate") java.time.LocalDate birthDate,
-                      @Param("email") String email);
+  Optional<User> findByEmail(String email);
 
 }
