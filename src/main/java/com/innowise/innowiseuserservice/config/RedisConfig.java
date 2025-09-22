23c0e0fee245b518b0ed99c7b59ada54264b15a4
Katya Remenchik <164913748+katermnchk@users.noreply.github.com>
2025-09-23 00:58:39 +0300

    added RedisConfig

---
+++ src/main/java/com/innowise/innowiseuserservice/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.innowise.innowiseuserservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.time.Duration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+@Configuration
+public class RedisConfig {
+  @Bean
+  public RedisCacheConfiguration cacheConfiguration() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+    objectMapper.activateDefaultTyping(
+        objectMapper.getPolymorphicTypeValidator(),
+        ObjectMapper.DefaultTyping.NON_FINAL
+    );
+
+    GenericJackson2JsonRedisSerializer serializer =
+        new GenericJackson2JsonRedisSerializer(objectMapper);
+
+    return RedisCacheConfiguration.defaultCacheConfig()
+        .entryTtl(Duration.ofHours(24))
+        .serializeValuesWith(
+            RedisSerializationContext.SerializationPair.fromSerializer(serializer)
+        );
+  }
+
+}
