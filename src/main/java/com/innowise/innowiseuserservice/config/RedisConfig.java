dedc3eca5e3b0847d554c44eaff92f561c65a15f
Katya Remenchik <164913748+katermnchk@users.noreply.github.com>
2025-09-26 00:14:31 +0300

    fix RedisConfig

--- src/main/java/com/innowise/innowiseuserservice/config/RedisConfig.java
+++ src/main/java/com/innowise/innowiseuserservice/config/RedisConfig.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -14,21 +15,23 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 public class RedisConfig {
 
   @Bean
+  @Primary
   public ObjectMapper objectMapper() {
     ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.registerModule(new JavaTimeModule());
     objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
-    objectMapper.activateDefaultTyping(
-        objectMapper.getPolymorphicTypeValidator(),
-        ObjectMapper.DefaultTyping.NON_FINAL
-    );
 
     return objectMapper;
   }
 
   @Bean
   public GenericJackson2JsonRedisSerializer redisSerializer(ObjectMapper objectMapper) {
+    ObjectMapper redisMapper = new ObjectMapper();
+    redisMapper.registerModule(new JavaTimeModule());
+    redisMapper.activateDefaultTyping(
+        redisMapper.getPolymorphicTypeValidator(),
+        ObjectMapper.DefaultTyping.NON_FINAL
+    );
     return new GenericJackson2JsonRedisSerializer(objectMapper);
   }
 
